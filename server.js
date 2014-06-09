1207be929986907eb3d5f01348dfd96e71050471
JichaoS <jichaos@andrew.cmu.edu>
2014-06-09 16:58:07 -0400

    Working verison

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ server = http.createServer(function(request, response){
   
   response.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});  
   var body = '';
+
   request.on('data', function(chunk){
     body += chunk;
-  }
+  });
 
   request.on('end', function(){
-    var config1 = "var config = {\njsonfile :'";
-    var config2 = "',\nanimation_delay : 800,\nwidth : 950,\nheight : 1000,\ninter_chart_margin: 25,\ncontextHeight : 100,\nmargin : {top: 50, right: 40, bottom: 50, left: 60}\n}";
-
-
     var d = new Date();
     dMonth = d.getMonth()+1;
     dDate = d.getDate();
@@ -28,14 +25,13 @@ server = http.createServer(function(request, response){
     dSeconds = d.getSeconds();
     dM = ((d.getHours()+1)<12 ? 'AM' : 'PM');
     df = dMonth + '-' + dDate + '-' + dYear +' '+dHour+"-"+dMinutes+'-'+dSeconds + dM;
-    json = "logs/"+ df +".json";
-    js = "logs/"+df+".js";
+    json =  "/logs/"+ df +".json";
 
-    fs.writeFile(json, body);
-    fs.writeFile(js, config1+json+config2);
-    response.write('callback(\'{\"msg\":\"' + js + '\"}\')');
+    fs.writeFile(__dirname + json, body + "\n");
+    response.end("http://gauss.modck.cs.cmu.edu/"+json);
+    body = '';
   });
-  response.end();
+  request.resume();
 });
 
 server.listen(8081);
